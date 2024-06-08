486015801ba5b42e584c679613134cc3064adde7
Aidan Gu <87115671+AidanGu@users.noreply.github.com>
2024-06-08 12:56:49 -0700

    pancake waffle

---
+++ pancake_waffle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+  bool made_in_waffle_iron = false;
+
+  if (made_in_waffle_iron = true) {
+    printf("waffle\n");
+  } else {
+    printf("pancake\n");
+  }
+
+  return 0;
+}
